ed6ef917ba4554492530e352080930c1b87a5dd1
Babariya Pratikkumar <69592394+pratik376@users.noreply.github.com>
2026-07-21 23:32:56 -0700

    Step 13: add_positional_encoding_to_embeddings

--- model.py
+++ model.py
@@ -182,8 +182,15 @@ def build_sinusoidal_positional_encoding(max_len, d_model):
 
     return positional_matrix
 
-# Step 13 - add_positional_encoding_to_embeddings (not yet solved)
-# TODO: implement
+# Step 13 - add_positional_encoding_to_embeddings
+import torch
+
+def add_positional_encoding_to_embeddings(embedded_batch, positional_encoding):
+    # TODO: add the first L rows of positional_encoding to embedded_batch and return the sum.
+    
+    B,L,d_model= embedded_batch.shape
+
+    return embedded_batch + positional_encoding[:L]
 
 # Step 14 - build_padding_mask (not yet solved)
 # TODO: implement

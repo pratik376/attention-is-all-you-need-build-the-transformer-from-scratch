fd61f9ebe27dd15edad5141533edd2e200c2db62
Babariya Pratikkumar <69592394+pratik376@users.noreply.github.com>
2026-07-29 23:33:26 -0700

    Step 65: update_adam_first_moment

--- model.py
+++ model.py
@@ -997,8 +997,14 @@ def initialize_adam_optimizer_state(parameter_list):
     
     return state
 
-# Step 65 - update_adam_first_moment (not yet solved)
-# TODO: implement
+# Step 65 - update_adam_first_moment
+import torch
+
+def update_adam_first_moment(m_prev, grad, beta1):
+    """Return m_t = beta1 * m_prev + (1 - beta1) * grad."""
+    # TODO: apply the Adam first-moment EMA update and return the new tensor
+
+    return beta1 * m_prev + (1-beta1)* grad
 
 # Step 66 - update_adam_second_moment (not yet solved)
 # TODO: implement
